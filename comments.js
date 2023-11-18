9761470746291fba1ae495dc1a35c0289aa227e9
nguyenthanhdon <150527763+nguyenthanhdon@users.noreply.github.com>
2023-11-18 03:11:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -13,21 +13,4 @@ var auth = require('./lib/auth.js');
 var topic = require('./lib/topic.js');
 var comments = require('./lib/comments.js');
 
-// 2. create web server object
-var app = http.createServer(function(request,response){
-    // 2.1 get url info.
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query; // query string
-    var pathname = url.parse(_url, true).pathname; // path name
-    var title = queryData.id; // title
-    
-    // 2.2 login status check
-    var isOwner = auth.isOwner(request, response);
-    var user = auth.getUser(request, response);
-    
-    // 2.3 routing
-    if(pathname === '/'){
-        if(queryData.id === undefined){
-            // 2.3.1 home
-            //
 
